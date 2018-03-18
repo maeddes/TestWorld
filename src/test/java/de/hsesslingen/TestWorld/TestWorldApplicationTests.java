45288d13529f5b2d4e53ffcbb64e6fd2bbf1c829
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2018-03-18 22:20:10 +0100

    first

---
+++ src/test/java/de/hsesslingen/TestWorld/TestWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package de.hsesslingen.TestWorld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
