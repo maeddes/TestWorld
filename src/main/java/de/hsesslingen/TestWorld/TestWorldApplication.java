7491b0ded5118520e4916158ac20d83b7862731a
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2018-04-15 14:45:55 +0200

    new version with JPA

--- src/main/java/de/hsesslingen/TestWorld/TestWorldApplication.java
+++ src/main/java/de/hsesslingen/TestWorld/TestWorldApplication.java
@@ -2,17 +2,9 @@ package de.hsesslingen.TestWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TestWorldApplication {
-
-	@GetMapping(){
-
-		
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestWorldApplication.class, args);
